b0b36c1b388de5494febcc4af9fc5b87e7c1eacf
Jiri Hajek <jiri.hajek18@gmail.com>
2020-06-09 10:18:11 +0200

    Add sorting and unit parameter to listLanguages()

--- main.go
+++ main.go
@@ -7,8 +7,10 @@ import (
 	"github.com/shurcooL/githubv4"
 	"golang.org/x/oauth2"
 	"log"
+	"math"
 	"os"
 	"sort"
+	"strings"
 )
 
 func authenticate(token string) (*githubv4.Client, error) {
@@ -37,7 +39,7 @@ func listReposWithLanguages(repos *[]repoEntry) {
 	for i, repo := range *repos {
 		fmt.Printf("Repository %s contains\n", repo.nameWithOwner)
 		for _, lang := range repo.langs {
-			fmt.Printf("%d bytes of %s\n", lang.bytes, lang.name)
+			fmt.Printf("%d B of %s\n", lang.bytes, lang.name)
 		}
 		if i < len(*repos)-1 {
 			fmt.Println()
@@ -61,15 +63,130 @@ func getLanguagesFromRepos(repos *[]repoEntry) *[]langEntry {
 	return &langSlice
 }
 
-func listLanguages(langs *[]langEntry, sortKey string, sortDirection string) {
-	if sortKey != "" && sortDirection != "" {
-		sort.Slice(*langs,
-			func(a, b int) bool { return (*langs)[a].bytes > (*langs)[b].bytes })
+func listLanguages(langs *[]langEntry, sortKey string, sortDirection string, unit string) {
+	var sortFunc func(a, b int) bool
+	switch sortKey {
+	case "name":
+		switch sortDirection {
+		case "ascending":
+			sortFunc = func(a, b int) bool {
+				return strings.Compare((*langs)[a].name, (*langs)[b].name) < 0
+			}
+		case "descending":
+			sortFunc = func(a, b int) bool {
+				return strings.Compare((*langs)[a].name, (*langs)[b].name) > 0
+			}
+		default:
+			log.Printf(
+				"Warning: unknown sort direction '%s' in listLanguages(), defaulting to 'ascending'\n",
+				sortDirection,
+			)
+			sortFunc = func(a, b int) bool {
+				return strings.Compare((*langs)[a].name, (*langs)[b].name) < 0
+			}
+		}
+	case "bytes":
+		switch sortDirection {
+		case "ascending":
+			sortFunc = func(a, b int) bool {
+				return (*langs)[a].bytes < (*langs)[b].bytes
+			}
+		case "descending":
+			sortFunc = func(a, b int) bool {
+				return (*langs)[a].bytes > (*langs)[b].bytes
+			}
+		default:
+			log.Printf(
+				"Warning: unknown sort direction '%s' in listLanguages(), defaulting to descending\n",
+				sortDirection,
+			)
+			sortFunc = func(a, b int) bool {
+				return (*langs)[a].bytes > (*langs)[b].bytes
+			}
+		}
+	case "":
+		sortFunc = nil
+	default:
+		log.Printf("Warning: unknown sort key '%s' in listLanguages()\n", sortKey)
+		sortFunc = nil
 	}
 
-	for _, lang := range *langs {
-		fmt.Printf("%s: %d bytes\n", lang.name, lang.bytes)
+	if sortFunc != nil {
+		sort.Slice(*langs, sortFunc)
 	}
+
+	totalSize := 0
+	for _, lang := range *langs {
+		totalSize += lang.bytes
+	}
+
+	totalSizeString := getAutoSize(totalSize)
+	fmt.Printf("Total size: %s\n", totalSizeString)
+
+	for _, lang := range *langs {
+		fmt.Printf("%s: %s\n", lang.name, getSizeByUnit(lang.bytes, unit))
+	}
+}
+
+func getSizeByUnit(size int, unit string) string {
+	var exp int
+	switch unit {
+	case "auto":
+		return getAutoSize(size)
+	case "B":
+		exp = 1
+	case "kB":
+		exp = -3
+	case "MB":
+		exp = -6
+	case "GB":
+		exp = -9
+	case "TB":
+		exp = -12
+	case "PB":
+		exp = -15
+	case "EB":
+		exp = -18
+	// no need for more units because 10^18 approaches the limits of 64bit integers
+	default:
+		log.Printf("Warning: unknown unit '%s' in getSizeByUnit(), defaulting to B\n", unit)
+		unit = "B"
+		exp = 1
+	}
+
+	return fmt.Sprintf("%f %s", float64(size)*math.Pow10(exp), unit)
+}
+
+func getAutoSize(size int) string {
+	unitNo := 0
+	var unit string
+	sizeFloat := float64(size)
+	for sizeFloat > 1000 {
+		sizeFloat = sizeFloat / 1000
+		unitNo++
+	}
+
+	switch unitNo {
+	case 0:
+		unit = "B"
+	case 1:
+		unit = "kB"
+	case 2:
+		unit = "MB"
+	case 3:
+		unit = "GB"
+	case 4:
+		unit = "TB"
+	case 5:
+		unit = "PB"
+	case 6:
+		unit = "EB"
+		// no need for more units because 10^18 approaches the limits of 64bit integers
+	default:
+		log.Fatal("Error in getAutoSize(): this shouldn't have happened because 64bit integers can't reach sizes larger than ~10^18")
+	}
+
+	return fmt.Sprintf("%f %s", sizeFloat, unit)
 }
 
 type queryFirstRepos struct {
@@ -223,5 +340,10 @@ func main() {
 	}
 
 	langs := getLanguagesFromRepos(repos)
-	listLanguages(langs, "bytes", "desc")
+	listReposWithLanguages(repos)
+	fmt.Println()
+	sortKey := "bytes"
+	sortDirection := "descending"
+	unit := "auto"
+	listLanguages(langs, sortKey, sortDirection, unit)
 }
