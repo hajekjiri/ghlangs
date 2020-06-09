80e0bf9ca0b7d141ffd640c41b788671882e6d68
Jiri Hajek <jiri.hajek18@gmail.com>
2020-06-09 23:42:47 +0200

    Fix typos in strpad, improve strpad documentation

--- strpad.go
+++ strpad.go
@@ -5,7 +5,9 @@ import (
 	"log"
 )
 
-// Strlpad pads a string on the left side by padLen
+// Strlpad pads a copy of the input string on the left side with spaces to
+// make its length equal to  padLen. If the string's initial length is greater
+// or equal to padLen, the same string is returned.
 func Strlpad(str string, padLen int) string {
 	result, err := strpad(str, padLen, "left")
 	if err != nil {
@@ -15,7 +17,9 @@ func Strlpad(str string, padLen int) string {
 	return result
 }
 
-// Strrpad pads a string on the left side by padLen
+// Strrpad pads a copy of the input string on the left side with spaces to
+// make its length equal to  padLen. If the string's initial length is greater
+// or equal to padLen, the same string is returned.
 func Strrpad(str string, padLen int) string {
 	result, err := strpad(str, padLen, "right")
 	if err != nil {
@@ -27,14 +31,16 @@ func Strrpad(str string, padLen int) string {
 
 func strpad(str string, padLen int, padDirection string) (string, error) {
 	if padLen <= len(str) {
-		return string(str), nil
+		return str, nil
 	}
 
+	// create byte slice of spaces
 	whitespace := make([]byte, padLen-len(str))
 	for i := range whitespace {
 		whitespace[i] = ' '
 	}
 
+	// append spaces to the input string
 	bytes := []byte(str)
 	switch padDirection {
 	case "left":
@@ -44,5 +50,6 @@ func strpad(str string, padLen int, padDirection string) (string, error) {
 	default:
 		return str, fmt.Errorf("strpad(): invalid pad direction '%s'", padDirection)
 	}
+
 	return string(bytes), nil
 }
