33f12d5eb0964d9bf87d10022f90c3e39e9e9cf6
Jiri Hajek <jiri.hajek18@gmail.com>
2020-06-19 18:05:13 +0200

    Add doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package ghlangs is a command-line tool utilizing the GitHub GraphQL API v4 to
+// tell you what programming languages are dominant in any user's or
+// oganization's GitHub repositories.
+//
+// See README for more details.
+package main // import "github.com/hajekjiri/ghlangs"
